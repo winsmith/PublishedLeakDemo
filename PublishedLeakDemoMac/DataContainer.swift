a22c6b9047c952d76910edd8e81a47581b34b985
Daniel Jilg <winsmith@winsmith.de>
2021-01-02 14:56:13 +0100

    Add readme and screenshots.

--- PublishedLeakDemoMac/DataContainer.swift
+++ PublishedLeakDemoMac/DataContainer.swift
@@ -1,10 +1,3 @@
-//
-//  DataContainer.swift
-//  PublishedLeakDemoMac
-//
-//  Created by Daniel Jilg on 02.01.21.
-//
-
 import Foundation
 import SwiftUI
 
@@ -12,7 +5,6 @@ class DataContainer: ObservableObject {
     @Published var someBoolean: Bool = false
 
     func toggleSomeBoolean() {
-
         for _ in 0...20 {
             someBoolean.toggle()
         }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Possible Leak in `@Published` on macOS
+
+This is a demo project that tries to reproduce a possible memory leak around `@Published` variables in SwiftUI macOS apps.
+
+![An animation showing the app's memory footprint increasing](leakanimation.gif)
