acdf4ad008e1c29ebb3bd8dc4c0613b5ee6d6317
Daniel Jilg <winsmith@winsmith.de>
2021-01-02 14:22:53 +0100

    Initial Commit

---
+++ PublishedLeakDemoMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PublishedLeakDemoMac
+//
+//  Created by Daniel Jilg on 02.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PublishedLeakDemoMac/PublishedLeakDemoMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishedLeakDemoMacApp.swift
+//  PublishedLeakDemoMac
+//
+//  Created by Daniel Jilg on 02.01.21.
+//
+
+import SwiftUI
+
+@main
+struct PublishedLeakDemoMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
